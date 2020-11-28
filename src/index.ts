b31b15df7df6febb31571b6752ee87e645bbf621
fixiabis <fixiabis@gmail.com>
2020-11-28 23:27:51 +0800

    feat: change to use sheet for run tasks

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,78 @@
+import Axios from 'axios';
 import Puppeteer, { Browser } from 'puppeteer';
 import PuppeteerExtra from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { blogger, analytics } from './actions';
 
-const options: Puppeteer.LaunchOptions = {
+const launchOptions: Puppeteer.LaunchOptions = {
   headless: false,
   ignoreHTTPSErrors: true,
   defaultViewport: { width: 1366, height: 768 },
   args: ['--start-maximized', '--no-sandbox', '--disable-popup-blocking'],
 };
 
-PuppeteerExtra.use(StealthPlugin()).launch(options).then(runTask);
+PuppeteerExtra.use(StealthPlugin());
 
-const identifierId = 'twmedicalrobot001';
-const password = '7wm3d1c41r0b07';
-const blogDisplayName = 'twmedicalrobot001';
-const blogTitle = 'twmedicalrobot001-00-15';
-const blogDomainPrefix = 'twmedicalrobot001-00-15';
-const analyticsAccountName = 'twmedicalrobot001';
-const analyticsWebsiteName = 'twmedicalrobot001';
-const analyticsWebsiteUrl = `${blogDomainPrefix}.blogspot.com`;
+const infoApiUrl =
+  'https://script.google.com/macros/s/AKfycbyZtHBX9sRYRt9-2A76oFt2ANWzV9HgPK2jMWSQfJmoGlIZVXc/exec?sheet=blogs';
 
-async function runTask(browser: Browser) {
+Axios.get(infoApiUrl)
+  .then(({ data }) => data)
+  .then(runTasks);
+
+interface BlogInfo {
+  title: string;
+  domainPrefix: string;
+  themeName: string;
+  account: string;
+  password: string;
+  blogUserName: string;
+}
+
+async function runTasks(blogInfos: BlogInfo[]) {
+  for (const blogInfo of blogInfos) {
+    await runTask(blogInfo);
+  }
+}
+
+async function runTask(blogInfo: BlogInfo) {
+  const browser = await PuppeteerExtra.launch(launchOptions);
   const page = await browser.newPage();
 
+  console.log('got info:', blogInfo);
+
   try {
-    await blogger.signIn(page, identifierId, password);
+    await blogger.signIn(page, blogInfo.account, blogInfo.password);
 
     const blogId = await blogger.createBlog(
       page,
-      blogTitle,
-      blogDomainPrefix,
-      blogDisplayName
+      blogInfo.title,
+      blogInfo.domainPrefix,
+      blogInfo.blogUserName
     );
 
     console.log(`blog id: ${blogId}`);
 
-    // const analyticsId = await analytics.createAnalytics(
-    //   page,
-    //   analyticsAccountName,
-    //   analyticsWebsiteName,
-    //   analyticsWebsiteUrl
-    // );
+    const analyticsId = await analytics.createAnalytics(
+      page,
+      blogInfo.domainPrefix,
+      blogInfo.domainPrefix,
+      `${blogInfo.domainPrefix}.blogspot.com`
+    );
 
-    // console.log(`analytics id: ${analyticsId}`);
+    console.log(`analytics id: ${analyticsId}`);
 
-    // const analyticsCode = await analytics.getCode(page, analyticsId);
+    const analyticsCode = await analytics.getCode(page, analyticsId);
 
-    // console.log(`analytics code: ${analyticsCode}`);
+    console.log(`analytics code: ${analyticsCode}`);
 
-    // await blogger.setBlogAnalyticsCode(page, blogId, analyticsCode);
+    await blogger.setBlogAnalyticsCode(page, blogId, analyticsCode);
+    await blogger.setBlogTheme(page, blogId, blogInfo.themeName);
   } catch (error) {
     console.log(error.message);
   } finally {
-    // await browser.close();
+    try {
+      await browser.close();
+    } catch (error) {}
   }
 }
